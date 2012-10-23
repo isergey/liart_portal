1785b2fa52b8d02e453e793037c1ec07a6e5a698
Sergey <dostovalov@gmail.com>
2012-10-23 17:26:59 +0400

    fix

--- libcms/apps/news/urls.py
+++ libcms/apps/news/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import *
 
+
 urlpatterns = patterns('',
     (r'^admin/', include('news.administration.urls', namespace='administration')),
     (r'^', include('news.frontend.urls', namespace='frontend')),
