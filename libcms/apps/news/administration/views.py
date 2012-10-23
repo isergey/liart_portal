e64805c185ece17460865a0a2d5e3f51732ccbd1
Sergey <dostovalov@gmail.com>
2012-10-23 17:33:19 +0400

    news improve

--- libcms/apps/news/administration/views.py
+++ libcms/apps/news/administration/views.py
@@ -44,7 +44,10 @@ def create_news(request):
                     avatar_img_name = handle_uploaded_file(request.FILES['news_form_avatar'])
                     news.avatar_img_name = avatar_img_name
                 news.save()
-                return redirect('news:administration:news_list')
+                if 'save_edit' in request.POST:
+                    return redirect('news:administration:edit_news', news.id)
+                else:
+                    return redirect('news:administration:news_list')
     else:
         news_form = NewsForm(prefix="news_form")
 
